fa2b790ac9aa28d1b8c02400acae4cc43d4088f9
eliseu <eliseu74123@gmail.com>
2024-05-15 08:11:26 -0300

    feito

---
+++ Exercício31.py
@@ -0,0 +1,9 @@
+distancia = float (input("qual é a distancia da viagem:"))
+
+n1 = distancia * 0.50
+n2 = distancia * 0.45
+
+if distancia > 200:
+    print(f"vai pagar RS${n2}")
+else:
+    print(f"vai pagar muito RS${n1}")
